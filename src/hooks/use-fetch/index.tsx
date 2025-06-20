2c913bee2b540efe7d5493b2c60dbae2ab1e319c
PedroRCSilva <pedro.rsilva@sptech.school>
2025-06-20 10:37:03 -0300

    feat: enhance useFetch hook to include refetch functionality

--- src/hooks/use-fetch/index.tsx
+++ src/hooks/use-fetch/index.tsx
@@ -4,5 +4,8 @@ import { QueryClient } from './query-client'
 export const useFetch = <T,>(promisseFn: () => Promise<T>) => {
   const [query] = useState(() => QueryClient(promisseFn))
   const data = useSyncExternalStore(query.subcribe, query.getState)
-  return data
+  return {
+    ...data,
+    refetch: query.refetch
+  }
 }
