c30b86b15849cbd76d0a81e5138cfd03f6d82761
PedroRCSilva <pedro.rsilva@sptech.school>
2025-06-20 10:36:56 -0300

    fix: handle case when category data is undefined in useCategory hook

--- src/pages/home/hooks/use-category/index.tsx
+++ src/pages/home/hooks/use-category/index.tsx
@@ -5,9 +5,11 @@ import { useFetch } from '@hooks/use-fetch'
 export const useCategory = () => {
   const { data } = useFetch(categoryClient.getCategory)
 
-  const categories = data?.data.map(element => ({
-    ...element,
-    type: element.type ? 'Gasto Fixo' : 'Gasto variável'
-  }))
+  const categories = data?.data
+    ? data.data.map(element => ({
+        ...element,
+        type: element.type ? 'Gasto Fixo' : 'Gasto variável'
+      }))
+    : []
   return { categories }
 }
