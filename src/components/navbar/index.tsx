f27fa53f5d465802a29afea46195b52d81ce8838
PedroRCSilva <pedro.rsilva@sptech.school>
2025-07-04 19:48:45 -0300

    Adds Navbar component

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,22 @@
+export const Navbar = () => {
+  return (
+    <header className="relative flex items-center justify-between">
+      <h2></h2>
+      {/* <div onClick={() => setIsOpen(prev => !prev)}>
+        <div className="flex cursor-pointer flex-col gap-1">
+          <hr className={clsx('w-5 transition', isOpen && 'translate-y-[5px] -rotate-45')} />
+          <hr className={clsx('w-5 transition', isOpen && '-rotate-45')} />
+          <hr className={clsx('w-5 transition', isOpen && '-translate-y-[5px] rotate-45')} />
+        </div>
+
+        <nav
+          className={clsx(
+            'fixed top-14 -right-[100%] z-10 h-[100vh] overflow-scroll bg-amber-300 px-4 py-6 transition-all',
+            isOpen && 'right-0'
+          )}>
+          <ul>Lista de Categorias</ul>
+        </nav>
+      </div> */}
+    </header>
+  )
+}
