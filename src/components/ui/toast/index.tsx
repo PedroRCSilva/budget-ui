4c6cb93ec1d5e5d98035294057526b04b2e62a0c
PedroMatrix <t-pedro.silva@matrixenergia.com>
2025-12-26 12:22:59 -0300

    refactor(ui): improve Toast component structure and readability

--- src/components/ui/toast/index.tsx
+++ src/components/ui/toast/index.tsx
@@ -1,65 +1,57 @@
-import { Check, Icon } from "lucide-react"
-import { HTMLAttributes, JSX, ReactNode, useState } from "react"
-import { tv, VariantProps } from "tailwind-variants"
+import { HTMLAttributes } from 'react'
+import { tv, VariantProps } from 'tailwind-variants'
 
 export const toastVariants = tv({
-    slots: {
-        container: "flex fixed gap-2 w-sm p-3 rounded-lg",
-        title: "font-medium",
-        description: "text-sm text-white font-light"
+  slots: {
+    container: 'flex fixed gap-2 w-sm p-3 rounded-lg',
+    title: 'font-medium',
+    description: 'text-sm text-white font-light'
+  },
+  defaultVariants: {
+    positionX: 'right',
+    positionY: 'top',
+    theme: 'success'
+  },
+  variants: {
+    theme: {
+      success: {
+        container: 'bg-green-900',
+        title: 'text-white',
+        description: 'text-white'
+      }
     },
-    defaultVariants:{
-        positionX:"right",
-        positionY:"top",
-        theme:"success"
+    positionX: {
+      left: {
+        container: 'left-2'
+      },
+      right: {
+        container: 'right-2'
+      }
     },
-    variants: {
-        theme: {
-            success: {
-                container: "bg-green-900",
-                title: "text-white",
-                description: "text-white"
-            }
-        },
-        positionX: {
-            left: {
-                container: "left-2"
-            },
-            right: {
-                container: "right-2"
-            }
-        },
-        positionY: {
-            top: {
-                container: "top-2"
-            },
-            bottom: {
-                container: "bottom-2"
-            }
-        }
+    positionY: {
+      top: {
+        container: 'top-2'
+      },
+      bottom: {
+        container: 'bottom-2'
+      }
     }
+  }
 })
 
-
 export const Toast: React.FC<{
-    variants?: VariantProps<typeof toastVariants>,
-    Icon?:React.FC<HTMLAttributes<HTMLElement>>
+  variants?: VariantProps<typeof toastVariants>
+  Icon?: React.FC<HTMLAttributes<HTMLElement>>
 }> = ({ variants, Icon }) => {
+  const { container, title, description } = toastVariants(variants)
 
-
-    const { container, title, description } = toastVariants(variants)
-    
-    return (
-
-        <div className={container()}>
-            <div className="flex items-center">
-                {Icon && <Icon className="text-white" />}
-            </div>
-            <div className="flex flex-col">
-                <span className={title()}>Login realizado com sucesso!</span>
-                <span className={description()}>Acesse seu site</span>
-            </div>
-        </div>
-    )
-
-}           
+  return (
+    <div className={container()}>
+      <div className="flex items-center">{Icon && <Icon className="text-white" />}</div>
+      <div className="flex flex-col">
+        <span className={title()}>Login realizado com sucesso!</span>
+        <span className={description()}>Acesse seu site</span>
+      </div>
+    </div>
+  )
+}
