869ef1863536a87f59d320fcbc1a2ec83bc7de82
PedroMatrix <t-pedro.silva@matrixenergia.com>
2025-12-27 15:20:06 -0300

    feat(toast): Add toast logic and type definitions

--- src/lib/toast/index.ts
+++ src/lib/toast/index.ts
@@ -1,27 +1,41 @@
 import { toastStore } from './context'
 import { IToastFn, ToastProps } from './types'
+import { IoIosCheckmarkCircle, IoMdWarning } from 'react-icons/io'
+
+const generateId = () => crypto.randomUUID()
 
 export const toast: IToastFn = {
-  warning: (data: ToastProps) =>
+  warning: (data: ToastProps) => {
+    const id = generateId()
     toastStore.add({
       ...data,
-      id: crypto.randomUUID(),
-      variants: {}
-    }),
+      id,
+      icon: IoIosCheckmarkCircle,
+      variants: {
+        theme: 'warning'
+      }
+    })
+  },
   success: (data: ToastProps) => {
+    const id = generateId()
     toastStore.add({
       ...data,
-      id: crypto.randomUUID(),
+      id,
+      icon: IoMdWarning,
       variants: {
         theme: 'success'
       }
     })
   },
   destructive: (data: ToastProps) => {
+    const id = generateId()
     toastStore.add({
       ...data,
-      id: crypto.randomUUID(),
-      variants: {}
+      id,
+
+      variants: {
+        theme: 'destructive'
+      }
     })
   },
   remove: (toastId: string) => toastStore.remove(toastId)
