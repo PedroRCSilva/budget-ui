e8655c4d6e78df1484c80d99ca306d32b9a8ad6e
PedroMatrix <t-pedro.silva@matrixenergia.com>
2025-12-27 15:20:06 -0300

    refactor(ui): Modernize Button component variants

--- src/components/ui/button/type.ts
+++ src/components/ui/button/type.ts
@@ -2,7 +2,6 @@ import { ReactNode } from 'react'
 
 export interface IButton
   extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
-  color?: 'primary' | 'secondary'
   isLoading?: boolean
   children?: ReactNode
 }
