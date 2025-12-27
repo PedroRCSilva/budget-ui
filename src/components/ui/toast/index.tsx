43c37e5ea4932350daea9dd10c61c0272dd6a903
PedroMatrix <t-pedro.silva@matrixenergia.com>
2025-12-27 15:20:06 -0300

    feat(ui): Implement new Toast notification system

--- src/components/ui/toast/index.tsx
+++ src/components/ui/toast/index.tsx
@@ -1,11 +1,15 @@
-import { HTMLAttributes } from 'react'
+import { toast } from '@lib/toast'
+import { X } from 'lucide-react'
+import { useCallback, useEffect, useState } from 'react'
 import { tv, VariantProps } from 'tailwind-variants'
+import { Button } from '../button'
+import { IconType } from 'react-icons'
 
 export const toastVariants = tv({
   slots: {
-    container: 'flex fixed gap-2 w-sm p-3 rounded-lg',
-    title: 'font-medium',
-    description: 'text-sm text-white font-light'
+    container: 'flex flex-col gap-2 w-sm p-3 rounded-lg',
+    titleText: 'font-medium',
+    descriptionText: 'text-sm text-white font-light'
   },
   defaultVariants: {
     positionX: 'right',
@@ -16,42 +20,68 @@ export const toastVariants = tv({
     theme: {
       success: {
         container: 'bg-green-900',
-        title: 'text-white',
-        description: 'text-white'
-      }
-    },
-    positionX: {
-      left: {
-        container: 'left-2'
+        titleText: 'text-white',
+        descriptionText: 'text-white'
       },
-      right: {
-        container: 'right-2'
-      }
-    },
-    positionY: {
-      top: {
-        container: 'top-2'
+      warning: {
+        container: 'bg-yellow-800',
+        titleText: 'text-white',
+        descriptionText: 'text-white'
       },
-      bottom: {
-        container: 'bottom-2'
+      destructive: {
+        container: 'bg-red-900',
+        titleText: 'text-white',
+        descriptionText: 'text-white'
       }
     }
   }
 })
 
 export const Toast: React.FC<{
+  id: string
   variants?: VariantProps<typeof toastVariants>
-  Icon?: React.FC<HTMLAttributes<HTMLElement>>
-}> = ({ variants, Icon }) => {
-  const { container, title, description } = toastVariants(variants)
+  durationMs?: number
+  hasLoading?: boolean
+  title?: string
+  description?: string
+  Icon?: IconType
+}> = ({ variants, Icon, id, durationMs = 2000, hasLoading, title, description }) => {
+  const { container, titleText, descriptionText } = toastVariants(variants)
+  const [state, setState] = useState<'open' | 'closed'>('open')
+
+  const handleClose = useCallback(() => {
+    setTimeout(() => toast.remove(id), 500)
+    setState('closed')
+  }, [id])
+
+  useEffect(() => {
+    const idTimeout = setTimeout(handleClose, durationMs)
+    return () => clearTimeout(idTimeout)
+  }, [durationMs, handleClose])
 
   return (
-    <div className={container()}>
-      <div className="flex items-center">{Icon && <Icon className="text-white" />}</div>
-      <div className="flex flex-col">
-        <span className={title()}>Login realizado com sucesso!</span>
-        <span className={description()}>Acesse seu site</span>
+    <div
+      className={container({
+        className: 'data-[state=open]:animate-toast-in data-[state=closed]:animate-toast-out animation-duration-[500ms]'
+      })}
+      data-state={state}>
+      <div className="flex gap-2">
+        <div className="flex items-center">{Icon && <Icon className="text-white" />}</div>
+        <div className="flex flex-1 flex-col">
+          <span className={titleText()}>{title}</span>
+          <span className={descriptionText()}>{description}</span>
+        </div>
+        <Button color="ghost" className="p-0" onClick={handleClose}>
+          <X size={20} className="cursor-pointer text-white" />
+        </Button>
       </div>
+      {hasLoading && (
+        <div
+          className={'animate-loading-toast h-1 w-full rounded-full bg-white'}
+          style={{
+            animationDuration: `${durationMs}ms`
+          }}></div>
+      )}
     </div>
   )
 }
