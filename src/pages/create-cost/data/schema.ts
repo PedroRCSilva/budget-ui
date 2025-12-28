557c428d47a710d12e67edfe693061171100783e
PedroRCSilva <pedrorochacardoso1908@gmail.com>
2025-12-28 14:14:59 -0300

    feat: Improve form validation messages for cost creation

--- src/pages/create-cost/data/schema.ts
+++ src/pages/create-cost/data/schema.ts
@@ -2,7 +2,19 @@ import { sanitizePrice } from '@utils/formatters'
 import z from 'zod'
 
 export const schemaCost = z.object({
-  name: z.string().refine(value => value.length >= 5, 'A descrição deve conter mais de 5 caracteres'),
-  amount: z.string().refine(value => sanitizePrice(value) > 0),
-  categoryId: z.string().refine(value => !!value, 'Informe a categoria')
+  name: z
+    .string({
+      message: 'Esse campo é obrigatório'
+    })
+    .refine(value => value.length >= 5, 'A descrição deve conter mais de 5 caracteres'),
+  amount: z
+    .string({
+      message: 'Esse campo é obrigatório'
+    })
+    .refine(value => sanitizePrice(value) > 0),
+  categoryId: z
+    .string({
+      message: 'Esse campo é obrigatório'
+    })
+    .refine(value => !!value, 'Informe a categoria')
 })
