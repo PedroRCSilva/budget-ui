9413c634f960768c0332e0c0cf4336256a5f8fd5
PedroRCSilva <pedrorochacardoso1908@gmail.com>
2025-12-28 12:10:25 -0300

    refactor(ui): Modernize Button component variants

--- src/components/ui/button/index.tsx
+++ src/components/ui/button/index.tsx
@@ -1,34 +1,43 @@
 import React from 'react'
-import { tv } from 'tailwind-variants'
+import { tv, VariantProps } from 'tailwind-variants'
 import { IButton } from './type'
 
-const variants = {
-  base: 'group px-4 py-4 border rounded-lg cursor-pointer font-medium disabled:bg-gray-200 disabled:border-gray-200 disabled:text-white disabled:cursor-not-allowed',
+const buttonVariants = tv({
+  slots: {
+    button:
+      'group px-4 py-4 border rounded-lg cursor-pointer font-medium disabled:bg-gray-200 disabled:border-gray-200 disabled:text-white disabled:cursor-not-allowed',
+    loading: 'absolute inset-0 flex items-center justify-center'
+  },
   variants: {
     color: {
-      primary: 'bg-primary text-white font-medium',
-      secondary: 'text-primary hover:bg-primary hover:text-white border-primary'
-    }
-  }
-}
-
-const button = tv(variants)
-
-const loading = tv({
-  base: 'absolute inset-0 flex items-center justify-center',
-  variants: {
-    color: {
-      primary: 'border-t-transparent border-r-transparent border-b-white',
-      secondary: 'border-t-transparent border-r-transparent border-b-white'
+      primary: {
+        button: 'bg-primary text-white font-medium',
+        loading: 'border-t-transparent border-r-transparent border-b-white'
+      },
+      secondary: {
+        button: 'text-primary hover:bg-primary hover:text-white border-primary',
+        loading: 'border-t-transparent border-r-transparent border-b-white'
+      },
+      ghost: {
+        button: 'border-0',
+        loading: 'border-t-transparent border-r-transparent border-b-white'
+      }
     }
   }
 })
 
-export const Button: React.FC<IButton> = ({ color = 'primary', isLoading, className, children, ...props }) => {
+export const Button: React.FC<IButton & VariantProps<typeof buttonVariants>> = ({
+  color,
+  isLoading,
+  className,
+  children,
+  ...props
+}) => {
+  const { button, loading } = buttonVariants({ color })
   return (
-    <button type="button" className={button({ className, color })} {...props}>
+    <button type="button" {...props} className={button({ className })}>
       {children}
-      {isLoading && <div className={loading({ color })} />}
+      {isLoading && <div className={loading()} />}
     </button>
   )
 }
